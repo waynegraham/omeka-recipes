e1f76812b21ab5c45bb400eb93f7e713cf4edca5
Wayne Graham <wayne.graham@virginia.edu>
2013-01-20 10:41:52 -0500

    fix for double-requiring version

--- lib/omeka-recipes.rb
+++ lib/omeka-recipes.rb
@@ -1,6 +1,5 @@
 require "capistrano"
 require "capistrano/cli"
 require "helpers"
-require "omeka-recipes/version"
 
 Dir.glob(File.join(File.dirname(__FILE__), 'omeka-recipes/*.rb')).sort.each { |f| load f }
