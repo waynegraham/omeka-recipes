955c32a40b29b43fdd41629304b79f4e20f27b56
David McClure <davidwilliammcclure@gmail.com>
2014-07-24 15:27:56 -0700

    Trying to fix git_clone.

--- lib/omeka-recipes/omeka.rb
+++ lib/omeka-recipes/omeka.rb
@@ -16,10 +16,7 @@ Capistrano::Configuration.instance.load do
 
       # If a branch is provided, check it out.
       if branch.to_s
-        Dir.chdir("#{current_path}/#{directory}/#{repo_name}") do
-          run "git fetch --quiet"
-          run "git checkout #{branch} --quiet"
-        end
+        run "cd #{current_path}/#{directory}/#{repo_name} && git fetch --quiet && git checkout #{branch} --quiet"
       end
 
     end
